7d3a33df883cef68272149b1a6d49c65bf530436
Yar <iaroslavklopov@vk.com>
2024-05-24 14:08:13 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/balans.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для balans.xaml
+    /// </summary>
+    public partial class balans : Window
+    {
+        public balans()
+        {
+            InitializeComponent();
+            Loaded += Balans_Loaded;
+        }
+
+        private void Balans_Loaded(object sender, RoutedEventArgs e)
+        {
+            BalanceTextBox.Focus();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
